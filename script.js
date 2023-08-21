368109f524aee08dd5a8f83bd956bdbeb3ef8fba
Sydney <proteimus@gmail.com>
2023-08-21 11:36:51 -0500

    1st commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const cars = ['European Made', 'Asian build', 'Munster Trucks', 'Summer swift', 'Winter Crisp', ]
+
+const newCars = cars.map(car => {
+  return `I wish to drive ${car} car.`
+})
+console.log(newCars);
